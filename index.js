c58cbdc2c0d440e0016bdad73ee0dcc86f0bc53a
Shane Lupton <slupton89@gmail.com>
2018-08-24 10:15:40 -0600

    completed handleDeleteItemClicked function

--- index.js
+++ index.js
@@ -83,9 +83,12 @@ function handleItemCheckClicked() {
 
 
 function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
   console.log('`handleDeleteItemClicked` ran')
+  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    delete STORE[itemIndex];
+    renderShoppingList();
+  });
 }
 
 // this function will be our callback when the page loads. it's responsible for
